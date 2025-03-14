d6d766c6cbf80d1103348efc1c9ac327daf0fb64
nathantd3 <nated33@gmail.com>
2025-03-13 22:30:27 -0700

    Push

--- FoodieTime/Controllers/AuthenticationController.cs
+++ FoodieTime/Controllers/AuthenticationController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodieTime.Data.Helpers.Constants;
+using FoodieTime.Data.Models;
+using FoodieTime.ViewModels.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodieTime.Controllers
 {
     public class AuthenticationController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public AuthenticationController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public async Task<IActionResult> Login()
         {
             return View();
@@ -11,6 +22,27 @@ namespace FoodieTime.Controllers
 
         public async Task<IActionResult> Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            var newUser = new User()
+            {
+                FullName = $"{registerVM.FirstName} {registerVM.LastName}",
+                Email = registerVM.Email,
+                UserName = registerVM.Email
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, AppRoles.User);
+                await _signInManager.SignInAsync(newUser, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
---
+++ FoodieTime/ViewModels/Authentication/RegisterVM.cs
@@ -0,0 +1,19 @@
+﻿namespace FoodieTime.ViewModels.Authentication
+{
+    public class RegisterVM
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterRestaurantVM
+    {
+        public string Restaurant { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
