320f9ae8b7bb43a6d4de1556465981c368029e24
nathantd3 <nated33@gmail.com>
2025-03-18 21:53:01 -0700

    Push

--- FoodieTime/Controllers/AuthenticationController.cs
+++ FoodieTime/Controllers/AuthenticationController.cs
@@ -1,24 +1,31 @@
 ﻿using FoodieTime.Data.Helpers.Constants;
 using FoodieTime.Data.Models;
 using FoodieTime.ViewModels.Authentication;
+using FoodieTime.ViewModels.Settings;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
-namespace FoodieTime.Controllers
+namespace CircleApp.Controllers
 {
     public class AuthenticationController : Controller
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
-        public AuthenticationController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AuthenticationController(UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
         public async Task<IActionResult> Login()
         {
             return View();
         }
+
 
         public async Task<IActionResult> Register()
         {
@@ -28,10 +35,8 @@ namespace FoodieTime.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterVM registerVM)
         {
-            if (ModelState.IsValid)
-            {
+            if (!ModelState.IsValid)
                 return View(registerVM);
-            }
 
             var newUser = new User()
             {
@@ -55,7 +60,75 @@ namespace FoodieTime.Controllers
                 await _signInManager.SignInAsync(newUser, isPersistent: false);
                 return RedirectToAction("Index", "Home");
             }
-            return View();
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePassword(UpdatePasswordVM updatePasswordVM)
+        {
+            if (updatePasswordVM.NewPassword != updatePasswordVM.ConfirmPassword)
+            {
+                TempData["PasswordError"] = "Passwords do not match";
+                TempData["ActiveTab"] = "Password";
+
+                return RedirectToAction("Index", "Settings");
+            }
+
+            var loggedInUser = await _userManager.GetUserAsync(User);
+            var isCurrentPasswordValid = await _userManager.CheckPasswordAsync(loggedInUser, updatePasswordVM.CurrentPassword);
+
+            if (!isCurrentPasswordValid)
+            {
+                TempData["PasswordError"] = "Current password is invalid";
+                TempData["ActiveTab"] = "Password";
+                return RedirectToAction("Index", "Settings");
+            }
+
+            var result = await _userManager.ChangePasswordAsync(loggedInUser, updatePasswordVM.CurrentPassword, updatePasswordVM.NewPassword);
+
+            if (result.Succeeded)
+            {
+                TempData["PasswordSuccess"] = "Password updated successfully";
+                TempData["ActiveTab"] = "Password";
+                await _signInManager.RefreshSignInAsync(loggedInUser);
+            }
+
+            return RedirectToAction("Index", "Settings");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProfile(UpdateProfileVM profileVM)
+        {
+            var loggedInUser = await _userManager.GetUserAsync(User);
+            if (loggedInUser == null)
+                return RedirectToAction("Login");
+
+            loggedInUser.FullName = profileVM.FullName;
+            loggedInUser.UserName = profileVM.UserName;
+
+            var result = await _userManager.UpdateAsync(loggedInUser);
+            if (!result.Succeeded)
+            {
+                TempData["UserProfileError"] = "User profile could not be updated";
+                TempData["ActiveTab"] = "Profile";
+            }
+
+            await _signInManager.RefreshSignInAsync(loggedInUser);
+            return RedirectToAction("Index", "Settings");
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
--- FoodieTime/ViewModels/Authentication/RegisterVM.cs
+++ FoodieTime/ViewModels/Authentication/RegisterVM.cs
@@ -5,13 +5,13 @@ namespace FoodieTime.ViewModels.Authentication
     public class RegisterVM
     {
         [Required(ErrorMessage = "First Name is required")]
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "First Name must be between two and 20 characters")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First Name must only contain letters")]
-        public string FirstName {  get; set; }
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "First Name must be between 2 and 50 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First Name must contain only letters")]
+        public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required")]
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "Last Name must be between two and 20 characters")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last Name must only contain letters")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last Name must be between 2 and 50 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last Name must contain only letters")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
@@ -25,5 +25,4 @@ namespace FoodieTime.ViewModels.Authentication
         [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; }
     }
-
 }
